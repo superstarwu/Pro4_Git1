cfeba7b498317a39a43f48154939418b70a125f9
starwu <starwu@atguigu.com>
2019-01-05 22:39:27 +0800

    测试分支

---
+++ src/main/java/com/atguigu/svn/Employee.java
@@ -0,0 +1,12 @@
+package com.atguigu.svn;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("这是hotfix");
+		System.out.println("这是hotfix");
+		System.out.println("这是hotfix");
+		System.out.println("这是hotfix");
+	}
+	
+}
