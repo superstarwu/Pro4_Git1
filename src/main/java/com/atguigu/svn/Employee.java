8c07278edb1d0413c40e012672438f1fa3859635
starwu <starwu@atguigu.com>
2019-01-05 23:14:12 +0800

    测试冲突1

--- src/main/java/com/atguigu/svn/Employee.java
+++ src/main/java/com/atguigu/svn/Employee.java
@@ -4,7 +4,7 @@ public class Employee {
 
 	public static void main(String[] args) {
 		System.out.println("这是hotfix");
-		System.out.println("这是hotfix");
+		System.out.println("这是hotfix.....right!!");
 		System.out.println("这是hotfix");
 		System.out.println("这是hotfix");
 	}
