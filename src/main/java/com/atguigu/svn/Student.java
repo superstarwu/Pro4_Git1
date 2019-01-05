32e074bca50ec697bb3226bd0641919c2d09ed3b
starwu <starwu@atguigu.com>
2019-01-05 22:12:29 +0800

    在Eclipse中提交文件到本地库

---
+++ src/main/java/com/atguigu/svn/Student.java
@@ -0,0 +1,21 @@
+package com.atguigu.svn;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("Student1....");
+		System.out.println("Student2....");
+		System.out.println("Student3....");
+		System.out.println("Student4....");
+		System.out.println("Student5....");
+		System.out.println("Student6....");
+		System.out.println("Student6....");
+		System.out.println("Student6....");
+		System.out.println("Student6....");
+		System.out.println("Student6....");
+		System.out.println("Student7....");
+		System.out.println("Student7....");
+		System.out.println("Student7....");
+		System.out.println("Studen7....");
+	}
+}
